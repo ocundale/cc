fc0c73802f3d007d0a6e2971f896f434dc21d75f
Oli Cundale <ocundale@gmail.com>
2014-07-18 17:27:10 +0100

    lil ie change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
-	$('#word1').css('margin-left', '-300px').animate({left:'50%', top:"10%"}, 1000, function() {
-		$('#word2').css('margin-left', '-300px').animate({left:'50%', top:"35%"}, 1000, function() {
-			$('#word3').css('margin-left', '-300px').animate({left:'50%', top:"60%"}, 1000);
+	$('#word1').css('margin-left', '-300px').animate({left:'50%', top:"10%"}, 800, function() {
+		$('#word2').css('margin-left', '-300px').animate({left:'50%', top:"35%"}, 800, function() {
+			$('#word3').css('margin-left', '-300px').animate({left:'50%', top:"60%"}, 800);
 			setTimeout(function() {
 				$('.word-swoop').addClass('swooped');
 				$('.logo').animate({ left:'5%' }, 2000, function() {
